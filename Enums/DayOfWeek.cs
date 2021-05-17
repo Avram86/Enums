0eb1421c3246e191d3667fa79c700cc6f1acf0eb
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-05-17 21:07:56 +0300

    Label not existent among enum

---
+++ Enums/DayOfWeek.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Enums
+{
+    public enum DayOfWeek
+    {
+        Monday=0,
+        //1
+        Tuesday,
+        //2
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+
+    }
+}
--- Enums/Program.cs
+++ Enums/Program.cs
@@ -6,7 +6,53 @@ namespace Enums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //DayOfWeek day = GetDayOfWeek(125);
+            DayOfWeek day = (DayOfWeek)125;
+
+            switch (day)
+            {
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                    Console.WriteLine("You need to work!!!!");
+                    break;
+
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("You are in weekend, enjoy");
+                    break;
+
+                default:
+                    Console.WriteLine($"{day} does not represent a valid value of the {typeof(DayOfWeek)}");
+                    break;
+            }
+            Console.WriteLine(day);
+
+
+            Console.WriteLine((int)DayOfWeek.Monday);
+            Console.WriteLine((int)DayOfWeek.Tuesday);
+            Console.WriteLine((int)DayOfWeek.Wednesday);
+            Console.WriteLine((int)DayOfWeek.Thursday);
+            Console.WriteLine((int)DayOfWeek.Friday);
+            Console.WriteLine((int)DayOfWeek.Saturday);
+            Console.WriteLine((int)DayOfWeek.Sunday);
+        }
+
+
+        public static DayOfWeek GetDayOfWeek(int day)
+        {
+            if (Enum.IsDefined(typeof(DayOfWeek), day))
+            {
+                //true, when day represents a valid label
+                return (DayOfWeek)day;
+            }
+            else
+            {
+                throw new ArgumentException($"{day} does not represent a valid value of the {typeof(DayOfWeek)}");
+            }
         }
     }
 }
